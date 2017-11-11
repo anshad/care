387a2e3fa83c899adec09f1dd421e4652c2ca781
Anshad Vattapoyil <anshad.musafir@gmail.com>
2017-11-11 13:33:52 +0530

    Add basic mat-toolabr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
@@ -15,7 +17,8 @@ import { environment } from '../environments/environment';
     NoopAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
