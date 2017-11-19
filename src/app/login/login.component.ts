11d0d69ea8a00f481e6a2543729e3a4ac18cf63e
Anshad Vattapoyil <anshad.musafir@gmail.com>
2017-11-19 08:58:20 +0530

    Materialize login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,32 +2,30 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-	selector: 'app-login',
-	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.scss'],
-	encapsulation: ViewEncapsulation.None
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
+    showNav = true;
+    loginForm = true;
+    otpEnabled = false;
 
-	constructor(private router: Router) { }
+    constructor(private router: Router) {}
 
-	ngOnInit() {
-	}
-	showNav = true;
-	loginForm = true;
-	otpEnabled = false;
+    ngOnInit() {}
 
-	login() {
-		this.router.navigate(['/hospitals']);
-	}
+    login() {
+        this.router.navigate(['/hospitals']);
+    }
 
-	createOtp() {
-		this.otpEnabled = true;
-	}
+    createOtp() {
+        this.otpEnabled = true;
+    }
 
-	createUser() {
-		this.loginForm = true;
-	}
-
-
+    createUser() {
+        this.loginForm = true;
+    }
+    registerUser() {}
 }
